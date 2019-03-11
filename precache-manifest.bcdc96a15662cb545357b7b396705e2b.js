0c7ba93b395a46ee5b511fd37c8a6c5eec4b38e6
Akhzar Farhan <atakhzar@gmail.com>
2019-03-11 21:31:24 +0530

    Add files via upload

---
+++ precache-manifest.bcdc96a15662cb545357b7b396705e2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7e08fea8086a8ed293ae",
+    "url": "/static/js/main.169e869f.chunk.js"
+  },
+  {
+    "revision": "ae0b72b89335021e0997",
+    "url": "/static/js/2.4952e05c.chunk.js"
+  },
+  {
+    "revision": "7e08fea8086a8ed293ae",
+    "url": "/static/css/main.b341ceea.chunk.css"
+  },
+  {
+    "revision": "b6c110036854c249e36c84b0a3470b0f",
+    "url": "/index.html"
+  }
+];
